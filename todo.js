29e5dfbc08bac516b57c50b50065e7ea1970c9e2
Manoj Vignesh <manojvignesh@live.com>
2020-12-19 12:56:10 +0530

    Refactored todo.js Made change to pass test

--- todo.js
+++ todo.js
@@ -1,134 +1,116 @@
-const operation = process.argv[2];
-const values = process.argv[3];
 const fs = require('fs');
+
+const operation = process.argv[2];
+const value = process.argv[3];
 const path = './todo.txt'
-let [month, date, year] = new Date().toLocaleDateString("en-US").split("/")
-let obj = {
+const [month, date, year] = new Date().toLocaleDateString("en-US").split("/")
+let todoList = {
     pending: [],
     done: []
-
 };
 
-switch (operation) {
-    case "help":
-        console.log("Usage :-");
-        console.log(`$ todo add "todo item"  # Add a new todo`);
-        console.log(`$ todo ls               # Show remaining todos`);
-        console.log(`$ todo del NUMBER       # Delete a todo`);
-        console.log(`$ todo done NUMBER      # Complete a todo`);
-        console.log(`$ todo help             # Show usage`);
-        console.log(`$ todo report           # Statistics`);
-        break;
-    case "add":
-        if (fs.existsSync(path)) {
-            //file exists
-            fs.readFile('todo.txt', (err, data) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    obj = JSON.parse(data);
-                    obj.pending.push({
-                        value: values
-                    });
-                    let json = JSON.stringify(obj);
-                    fs.writeFile('todo.txt', json, function (err, result) {
-                        if (err) console.log('error', err);
-                        else console.log(`Added todo: "${values}"`);
-                    });
-                }
-            });
-        }
-        else {
-            //file not exist
-            obj.pending.push({
-                value: process.argv[3]
-            });
-
-            let json = JSON.stringify(obj);
-            fs.writeFile('todo.txt', json, () => {
-                console.log(`Added todo: "${values}"`);
-            });
-
-        }
-        break;
-    case "ls":
-        fs.readFile('todo.txt', (err, data) => {
-            if (err) {
-                console.log(err);
-            } else {
-                obj = JSON.parse(data);
-                if (obj.pending.length == 0) {
-                    console.log(" LIST empty");
-                }
-                else {
-                    for (let i = obj.pending.length - 1; i >= 0; i--) {
-                        console.log(`[${i + 1}]` + " " + obj.pending[i].value);
-
-                    }
-                }
-            }
-        });
-        break;
-    case "report":
-        fs.readFile('todo.txt', (err, data) => {
-            if (err) {
-                console.log(err);
-            } else {
-                obj = JSON.parse(data);
-                console.log(`${date + "/" + month + "/" + year}  Pending : ${obj.pending.length}  Completed : ${obj.done.length}`);
-            }
-        });
-        break;
-
-    case "del":
-        fs.readFile('todo.txt', (err, data) => {
-            if (err) {
-                console.log(err);
-            } else {
-                obj = JSON.parse(data);
-                if (process.argv[3] <= obj.pending.length) {
-                    obj.pending.splice(values - 1, 1);
-                    let json = JSON.stringify(obj);
-                    fs.writeFile('todo.txt', json, function (err, result) {
-                        if (err) console.log('error', err);
-                        else console.log(`Deleted todo  #${values}`);
-                    });
-                }
-                else {
-                    console.log(`Error: todo #${values} does not exist. Nothing deleted.`);
-                }
-
-            }
-        });
-        break;
-    case "done":
-        fs.readFile('todo.txt', (err, data) => {
-            if (err) {
-                console.log(err);
-            } else {
-                obj = JSON.parse(data);
-                if (process.argv[3] <= obj.pending.length) {
-                    obj.done.push({
-                        value: obj.pending[values - 1].value
-                    });
-                    obj.pending.splice(values - 1, 1);
-
-                    let json = JSON.stringify(obj);
-                    fs.writeFile('todo.txt', json, function (err, result) {
-                        if (err) console.log('error', err);
-                        else console.log(`Marked todo: #${values} as done.`);
-                    });
-                }
-                else {
-                    console.log(`Error: todo #${values} does not exist.`);
-                }
-
-            }
-        });
+initializeTodoListFromFile();
+performTodoOperation();
+storeTodoListToFile();
 
 
-        break;
-    default:
-        break;
+function initializeTodoListFromFile() {
+    if (fs.existsSync(path)) {
+        const data = fs.readFileSync(path);
+        todoList = JSON.parse(data);
+    }
 }
 
+function performTodoOperation() {
+    switch (operation) {
+        case "help":
+            printHelp();
+            break;
+        case "add":
+            addTodo();
+            break;
+        case "ls":
+            listTodo();
+            break;
+        case "del":
+            deleteTodo();
+            break;
+        case "done":
+            markTodoAsDone();
+            break;
+        case "report":
+            reportTodo();
+            break;
+        default:
+            printHelp();
+            break;
+    }
+}
+
+function printHelp() {
+    console.log(`Usage :-
+$ ./todo add "todo item"  # Add a new todo
+$ ./todo ls               # Show remaining todos
+$ ./todo del NUMBER       # Delete a todo
+$ ./todo done NUMBER      # Complete a todo
+$ ./todo help             # Show usage
+$ ./todo report           # Statistics`);
+}
+
+function addTodo() {
+    if (value) {
+        todoList.pending.push(value);
+        console.log(`Added todo: "${value}"`);
+    } else {
+        console.log("Error: Missing todo string. Nothing added!");
+    }
+}
+
+function listTodo() {
+    if (todoList.pending.length === 0) {
+        console.log("There are no pending todos!");
+    } else {
+        for (let i = todoList.pending.length - 1; i >= 0; i--) {
+            console.log(`[${i + 1}]` + " " + todoList.pending[i]);
+
+        }
+    }
+}
+
+function deleteTodo() {
+    if (value) {
+        const delValue = Number(value);
+        if (delValue && delValue <= todoList.pending.length) {
+            todoList.pending.splice(delValue - 1, 1);
+            console.log(`Deleted todo #${value}`);
+        } else {
+            console.log(`Error: todo #${value} does not exist. Nothing deleted.`);
+        }
+    } else {
+        console.log("Error: Missing NUMBER for deleting todo.");
+    }
+}
+
+function markTodoAsDone() {
+    if (value) {
+        const doneValue = Number(value);
+        if (doneValue && doneValue <= todoList.pending.length) {
+            todoList.done.push(todoList.pending[doneValue - 1]);
+            todoList.pending.splice(doneValue - 1, 1);
+
+            console.log(`Marked todo #${value} as done.`);
+        } else {
+            console.log(`Error: todo #${value} does not exist.`);
+        }
+    } else {
+        console.log("Error: Missing NUMBER for marking todo as done.");
+    }
+}
+
+function reportTodo() {
+    console.log(`${year + "-" + month + "-" + date} Pending : ${todoList.pending.length} Completed : ${todoList.done.length}`);
+}
+
+function storeTodoListToFile() {
+    fs.writeFileSync(path, JSON.stringify(todoList));
+}
